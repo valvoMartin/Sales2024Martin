e4b6f97683e5b90e07ace7a505c093134f341fa6
gustin valvo <maartinvalvo@gmail.com>
2024-02-17 15:05:34 -0300

    Conecto DB y creo tabla country

--- Sales.API/Program.cs
+++ Sales.API/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.EntityFrameworkCore;
+using Sales.API.Data;
+using System.Threading.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=ConnectionSA"));
+
 
 var app = builder.Build();
 
---
+++ Sales.Shared/Entities/Country.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sales.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "País")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        [MaxLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} caractéres")]
+        public string Name { get; set; } = null!;
+
+        //public ICollection<State>? States { get; set; }
+
+        //public int StatesNumber => States == null ? 0 : States.Count;
+    }
+}
